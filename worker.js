7bbd2e6bce5896911a94544cfce3bf894af093a6
akaracsony <akaracsony@gmail.com>
2020-05-15 15:58:02 -0400

    Fixed typo

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ onmessage = function (e) {
   console.log('Worker: Message received from main script');
   let result = e.data[0] * e.data[1];
   if (isNaN(result)) {
-    postMessage('Please write two numbers in here');
+    postMessage('Please write two numbers right in here');
   } else {
     let workerResult = 'Result: ' + result;
     console.log('Worker: Posting message back to main script');
