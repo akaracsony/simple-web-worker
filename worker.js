aef803f3052ca4e0110703dc63cb7c43cf5ed729
akaracsony <akaracsony@gmail.com>
2020-05-15 16:12:13 -0400

    New message

--- worker.js
+++ worker.js
@@ -1,8 +1,8 @@
-onmessage = function (e) {
+onmessage = e => {
   console.log('Worker: Message received from main script');
   let result = e.data[0] * e.data[1];
   if (isNaN(result)) {
-    postMessage('Please write two numbers right in here');
+    postMessage('Please write two numbers');
   } else {
     let workerResult = 'Result: ' + result;
     console.log('Worker: Posting message back to main script');
